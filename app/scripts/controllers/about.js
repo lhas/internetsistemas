c93efde68741cceabcfee0f98e9a9c3c203e7dec
lhas <luizhrqas@gmail.com>
2016-07-15 18:31:09 -0300

    Yeoman Angular com Grunt.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internetsistemasApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the internetsistemasApp
+ */
+angular.module('internetsistemasApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internetsistemasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the internetsistemasApp
+ */
+angular.module('internetsistemasApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
